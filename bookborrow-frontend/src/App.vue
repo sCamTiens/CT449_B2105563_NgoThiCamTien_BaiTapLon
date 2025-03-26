1c20cd0a8ce9a29b58eed3f54b58f18a4f751661
sCamTiens <thicamtien2003@gmail.com>
2025-03-26 23:23:36 +0700

    Cap nhat hien thi fontend

--- bookborrow-frontend/src/App.vue
+++ bookborrow-frontend/src/App.vue
@@ -1,31 +1,46 @@
 <script>
 import AppHeader from '@/components/AppHeader.vue';
+import SideBar from '@/components/SideBar.vue';
 
 export default {
   components: {
     AppHeader,
+    SideBar,
   },
+  computed: {
+    isLoggedIn() {
+      return !!localStorage.getItem("nhanvien");
+    }
+  }
 };
 </script>
 <template>
   <div id="app">
     <AppHeader />
-    <div class="container mt-3">
-      <router-view />
+    <div class="main mt-3">
+      <div class="row">
+        <!-- Chỉ hiển thị Sidebar nếu đã đăng nhập -->
+        <div class="col-md-3" v-if="isLoggedIn">
+          <SideBar />
+        </div>
+
+        <!-- Phần nội dung chiếm toàn bộ nếu chưa đăng nhập -->
+        <div :class="isLoggedIn ? 'col-md-9' : 'col-md-12'">
+          <router-view />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-/* Để trang rộng 100% */
-body {
-  width: 100%;
-  margin: 0;
-  padding: 0;
-}
-
 .page {
   max-width: 100%;
   margin: auto;
+}
+
+.main {
+  padding: 20px;
+  padding-right: 100px;
 }
 </style>
--- bookborrow-frontend/src/main.js
+++ bookborrow-frontend/src/main.js
@@ -1,10 +1,40 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/main.css";
+import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import router from "./router";
-createApp(App).use(router).mount("#app");
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import {
+  faBook,
+  faBuilding,
+  faUser,
+  faBookOpenReader,
+  faUsers,
+  faMagnifyingGlass,
+  faRedo,
+  faTrash,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faBook,
+  faBuilding,
+  faUser,
+  faBookOpenReader,
+  faUsers,
+  faMagnifyingGlass,
+  faRedo,
+  faTrash,
+  faRightFromBracket
+);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router).mount("#app");
