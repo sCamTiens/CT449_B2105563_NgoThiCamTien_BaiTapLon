3850263ca2d7d731c506583e2c83926636c2b3d5
sCamTiens <thicamtien2003@gmail.com>
2025-03-26 23:23:11 +0700

    Cap nhat file

--- bookborrow-backend/app/utils/mongodb.util.js
+++ bookborrow-backend/app/utils/mongodb.util.js
@@ -1,30 +1,11 @@
-// const { MongoClient } = require("mongodb");
-
-// class MongoDB {
-//   static connect = async (uri) => {
-//     if (this.client) return this.client;
-//     this.client = await MongoClient.connect(uri);
-//     return this.client;
-//   };
-// }
-
-// module.exports = MongoDB;
-
 const { MongoClient } = require("mongodb");
 
 class MongoDB {
-  // Sử dụng một thuộc tính static cho client
-  static client;
-
-  // Phương thức kết nối tới MongoDB
-  static connect = async (uri) => {
-    if (this.client) return this.client; // Nếu đã có client, trả về client hiện tại
-    this.client = await MongoClient.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    return this.client;
-  };
+    static connect = async (uri) => {
+        if (this.client) return this.client;
+        this.client = await MongoClient.connect(uri);
+        return this.client;
+    }
 }
 
 module.exports = MongoDB;
